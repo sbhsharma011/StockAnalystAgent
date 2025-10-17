591a9776108cd27ac00a69140b6c51b2608fee55
sbhsharma011 <41360123+sbhsharma011@users.noreply.github.com>
2025-10-17 11:29:23 -0500

    Update crew.py

--- crew.py
+++ crew.py
@@ -1,8 +1,8 @@
 from crewai import Crew
-from tasks.stock_analysis_task import stock_analysis_task
-from tasks.stock_trading_task import stock_trading_task
-from agents.stock_analyst_agent import stock_analyst_agent
-from agents.stock_trader_agent import stock_trader_agent
+from stockanalystagent.tasks.stock_analysis_task import stock_analysis_task
+from stockanalystagent.tasks.stock_trading_task import stock_trading_task
+from stockanalystagent.agents.stock_analyst_agent import stock_analyst_agent
+from stockanalystagent.agents.stock_trader_agent import stock_trader_agent
 
 #load_dotenv(".env")
 
@@ -10,4 +10,5 @@ stock_crew = Crew(
     agents=[stock_analyst_agent,stock_trader_agent],
     tasks=[stock_analysis_task,stock_trading_task],
     verbose=False
+
 )
